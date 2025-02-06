0212aca46cb8c0e5743762a22331b136fd81e4da
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-02-05 22:03:18 -0500

    intial commit

---
+++ Supplement_p1_3/Supplement_p1_3.cs
@@ -0,0 +1,32 @@
+﻿namespace Supplement_p1_3;
+
+public class Supplement_p1_3
+{
+    public static void CreateAsciiFile(string filePath, string content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void CreateBinaryFile(string filePath, byte[] content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static string ReadAsciiFile(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static byte[] ReadBinaryFile(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+    public static void CompareFileSizes()
+    {
+        throw new NotImplementedException();
+    }
+    private static string GenerateRandomString(int length)
+    {
+        throw new NotImplementedException();
+    }
+}
